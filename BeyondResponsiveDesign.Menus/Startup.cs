f6c07f4e3d4d241f575e07f9f6870bdc99d4d13e
Pete Smith <roysvork@gmail.com>
2014-08-02 10:46:07 +0100

    Initial commit - Basic layout

---
+++ BeyondResponsiveDesign.Menus/Startup.cs
@@ -0,0 +1,12 @@
+﻿namespace BeyondResponsiveDesign.Menus
+{
+    using Owin;
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseStaticFiles();
+        }
+    }
+}
