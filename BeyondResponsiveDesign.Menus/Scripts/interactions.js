fa0972a71520a3eec3960721498c7547899659da
Pete Smith <roysvork@gmail.com>
2014-08-14 15:26:38 +0100

    Toolbar is now visible all the time, and added basic content tabbing

--- BeyondResponsiveDesign.Menus/Scripts/interactions.js
+++ BeyondResponsiveDesign.Menus/Scripts/interactions.js
@@ -8,10 +8,32 @@
             interactions.HidePopups(e);
             interactions.ToggleSideBar(e);
         });
+
+        $(document.body).on('click', 'article>section>nav>ul>li.toolbar1', function (e) {
+            $(this).trigger('blur');
+            e.preventDefault();
+            e.stopPropagation();
+
+            $("#contentOne").removeClass("offLeft");
+            $("#contentOne").addClass("active");
+            //$("#contentTwo").removeClass("active");
+            $("#contentTwo").addClass("offRight");
+        });
+
+        $(document.body).on('click', 'article>section>nav>ul>li.toolbar2', function (e) {
+            $(this).trigger('blur');
+            e.preventDefault();
+            e.stopPropagation();
+
+            $("#contentOne").addClass("offLeft");
+            //$("#contentOne").removeClass("active");
+            $("#contentTwo").removeClass("offRight");
+            $("#contentTwo").addClass("active");
+        });
     };
 
     interactions.ToggleSideBar = function (e) {
-        if ($('body').hasClass('sidebar') || !$('body').hasClass('sidebarCollapse')) {
+        if ($('body').hasClass('sidebar')) {
             interactions.HideSideBar(e);
         } else {
             interactions.ShowSideBar(e);
@@ -20,13 +42,11 @@
 
     interactions.ShowSideBar = function (e) {
         $('body').addClass('sidebar');
-        $('body').removeClass('sidebarCollapse');
         $(window).trigger('resize');
     };
 
     interactions.HideSideBar = function (e) {
         $('body').removeClass('sidebar');
-        $('body').addClass('sidebarCollapse');
         $(window).trigger('resize');
         return false;
     };
