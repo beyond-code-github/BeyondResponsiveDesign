6052cdf69a8137e0fbd15d19d08dfb6e5d965c97
Pete Smith <roysvork@gmail.com>
2014-08-20 12:15:32 +0100

    Fixed major elastic scroll bug on iOS and minor fixes to agenda screen after rework

--- BeyondResponsiveDesign.Menus/Scripts/interactions.js
+++ BeyondResponsiveDesign.Menus/Scripts/interactions.js
@@ -4,19 +4,43 @@
         $.detectSwipe.preventDefault = false;
         interactions.navigate("home");
         
-        $('body').on('touchmove', function (e) {
-            var searchTerms = '.vscroll, .vscroll-scroll, .hscroll',
-                $target = $(e.target),
-                parents = $target.parents(searchTerms);
-
-            if (parents.length || $target.hasClass(searchTerms)) {
-                // ignore as we want the scroll to happen
-                // (This is where we may need to check if at limit)
-            } else {
-                e.preventDefault();
-            }
+        // Disable scroll for the document, we'll handle it ourselves
+        $(document).on('touchmove', function (e) {
+            e.preventDefault();
         });
 
+        // Check if we should allow scrolling up or down
+        $(document.body).on("touchstart", ".vscroll", function (e) {
+            // If the element is scrollable (content overflows), then...
+            if (this.scrollHeight !== this.clientHeight) {
+                // If we're at the top, scroll down one pixel to allow scrolling up
+                if (this.scrollTop === 0) {
+                    this.scrollTop = 1;
+                }
+                // If we're at the bottom, scroll up one pixel to allow scrolling down
+                if (this.scrollTop === this.scrollHeight - this.clientHeight) {
+                    this.scrollTop = this.scrollHeight - this.clientHeight - 1;
+                }
+            }
+            // Check if we can scroll
+            this.allowUp = this.scrollTop > 0;
+            this.allowDown = this.scrollTop < (this.scrollHeight - this.clientHeight);
+            this.lastY = e.originalEvent.pageY;
+        });
+
+        $(document.body).on('touchmove', ".vscroll", function (e) {
+            var event = e.originalEvent;
+            var up = event.pageY > this.lastY;
+            var down = !up;
+            this.lastY = event.pageY;
+
+            if ((up && this.allowUp) || (down && this.allowDown)) {
+                event.stopPropagation();
+            } else {
+                event.preventDefault();
+            }
+        });
+        
         window.onresize = function () {
             $(document.body).width(window.innerWidth).height(window.innerHeight);
         }
